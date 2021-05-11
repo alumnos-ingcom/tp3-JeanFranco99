fdf05ebe98d80276e06e87e2a62c26cd35aa63c7
jean franco <favreaujeanfranco@gmail.com>
2021-05-11 13:04:18 -0300

    Cambios en el script, ahora tienen echo

--- favreau_jean_grep1.sh
+++ favreau_jean_grep1.sh
@@ -1,6 +1,10 @@
+ echo "Imprimir todas las lineas que contengan un numero"
  grep -Ein --color 'x[[:digit:]]{4}' grepdata.txt
+ echo "Imprimir las lineas que comiencen con tres digitos seguidos de un espacio"
  grep -E --color '^[[:digit:]]{3}* '  grepdata.txt
 
 
+ echo "Imprimir todas las lineas que no comiencen con una S mayuscula"
  grep -v '^S' grepdata.txt
+ echo "imprimir las lineas que contengan una direccion de correo electronico"
  grep -Ei --color '[A-Z 0-9]+@[A-Z 0-9]+\.[A-Z]{2,4}' grepdata.txt
